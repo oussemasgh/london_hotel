1150312e0635c4e00496cc5b9c52b7a780c46f68
oussama sghaier <oussema.sghaier@etudiant-enit.utm.tn>
2024-06-30 12:39:10 +0200

    publish project for the first time

---
+++ src/main/java/com/oussama/london_hotel/LondonHotelApplication.java
@@ -0,0 +1,13 @@
+package com.oussama.london_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LondonHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LondonHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oussama/london_hotel/LondonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussama.london_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
